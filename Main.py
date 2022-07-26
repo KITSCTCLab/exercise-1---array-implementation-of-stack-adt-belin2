05597ee483119d08487c72ac5fafed3ff24413f2
Belin <100009546+belin2@users.noreply.github.com>
2022-07-26 22:49:13 +0530

    .

--- Main.py
+++ Main.py
@@ -1,43 +1,39 @@
 import os
-class Stack:
-    def __init__(self, size):
-        self.items = []
-        self.size = size
-
-    def is_empty(self):
-        # Write code here
-        if self.top==-1:
-            return 1
+class stackADT:
+    def __init__ (s,n):
+        s.n= n
+        s.st= [None]*n
+        s.top= -1
+    def push(s):
+        if s.top == s.n-1:
+            print("Stack Full\n")
         else:
-            return 0
-    def is_full(self):
-        # Write code here
-        if self.top==(self.size -1):
-            return 1
+            s.top+=1
+            data=int(input())#"enter the data to be pushed: "
+            s.st[s.top]=data
+    def display(s):
+        for i in range(s.top+1):
+            print(s.st[i],end="\n")
+    def pop(s):
+        if s.top != -1:
+            s.top=s.top-1
         else:
-            return 0
+            print("Empty Stack")
+        
+    def peek(s):
+            print(s.st[s.top],end="")
 
-    def push(self, data):
-        if not self.is_full():
-            self.top +=1
-            self.item[self.top0=data
 
-    def pop(self):
-        if not self.is_empty():
-            del self.items[self.top]
-            self.top -=1
-    def status(self):
-        # Write code here
-        if sef.is_empty() !=1:
-            for i in range(0,self.top+1):
-                print(self.items[i])
-# Do not change the following code
-size, queries = map(int, input().rstrip().split())
-stack = Stack(size)
-for line in range(queries):
-    values = list(map(int, input().rstrip().split()))
-    if values[0] == 1:
-        stack.push(values[1])
-    elif values[0] == 2:
-        stack.pop()
-stack.status()
+n=int(input())
+s1=stackADT(n)
+q = int(input())
+for i in range(q):
+    ch=int(input(""))#Enter the option: 
+    if ch==1:
+        s1.push()
+    elif ch==2:
+        s1.pop()
+    elif ch==3:
+        s1.peek()
+
+s1.display()
